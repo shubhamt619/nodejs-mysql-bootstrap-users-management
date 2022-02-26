ad947e46dd53d8434f972c42eee1426dc2e42b07
Aman Thakur <67583037+amanRT@users.noreply.github.com>
2022-02-26 13:01:26 +0530

    Testingh

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,18 +1,25 @@
-const user = require('../models').user
-module.exports = {
+const user = require("../models").user;
+import Sequelize from "sequelize";
+const sequelize = new Sequelize("node_ums", "root", "");
+const { QueryTypes } = require("@sequelize/core");
 
-    getDasboard: async function(req, res){
-        user.findAll({
-            where: {
-              id: 3
-            }
-          }).then(function(students) {
-            // students will be an array of all Project instances
-            let counts = {
-                'studentsCount': 10,
-                'adminsCount': 100,
-            }
-            res.render('admin/index', counts);
-        })
-    }
-}
+module.exports = {
+  getDasboard: async function (req, res) {
+    const users = await sequelize
+      .query(
+        "SELECT (SELECT COUNT(*) FROM users WHERE role =  1) AS studentsCount, (SELECT COUNT(*) FROM users WHERE role =  2) AS adminsCount",
+        {
+          type: QueryTypes.SELECT,
+        }
+      )
+      .then(function (result) {
+        console.log(result);
+        // students will be an array of all Project instances
+        let counts = {
+          studentsCount: result.studentsCount,
+          adminsCount: result.adminsCount,
+        };
+        res.render("admin/index", counts);
+      });
+  },
+};
